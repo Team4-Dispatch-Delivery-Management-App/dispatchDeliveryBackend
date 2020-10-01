3e1a3a09ccc4e8db0dd6e32a08b0111a519a95ce
Ruizi Zhang <39425360+Ruiziz@users.noreply.github.com>
2020-10-01 01:27:21 -0400

    Add History Record function + fix some bugs
    What we achieved for this version (Tested via Postman):
    
    1. Track all history order records by UserId
    2. Place Order by OptionId and UserId
    3. Registration and Login Function works well

--- src/main/java/dispatchApp/dao/OrderDao.java
+++ src/main/java/dispatchApp/dao/OrderDao.java
@@ -1,10 +1,18 @@
 package dispatchApp.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import dispatchApp.model.Account;
 import dispatchApp.model.Order;
 
 @Repository
@@ -29,7 +37,7 @@ public class OrderDao {
 			}
 		}
 	}
-	
+
 	public Order getOrderById(int orderId) {
 		try (Session session = sessionFactory.openSession()) {
 			session.beginTransaction();
@@ -41,5 +49,25 @@ public class OrderDao {
 		}
 		return null;
 	}
-}
 
+	
+
+	public List<Order> getOrderByUserId(int userId) {
+		List<Order> res = new ArrayList<>();
+		try (Session session = sessionFactory.openSession()) {
+			session.beginTransaction();
+			CriteriaBuilder builder = session.getCriteriaBuilder();
+			CriteriaQuery<Order> criteriaQuery = builder.createQuery(Order.class);
+			Root<Order> root = criteriaQuery.from(Order.class);
+			criteriaQuery.select(root).where(builder.equal(root.get("externalUserId"), userId)); 
+			res =  session.createQuery(criteriaQuery).getResultList();
+			
+			session.getTransaction().commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return res;
+	}
+	
+}
