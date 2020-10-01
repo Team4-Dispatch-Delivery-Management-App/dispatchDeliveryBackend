b0fc1a87ee81eeea834195f9b86c483dfa282541
wikiwu24 <70457942+wikiwu24@users.noreply.github.com>
2020-09-30 21:32:19 -0400

    Update LoginController.java

--- src/main/java/dispatchApp/controller/LoginController.java
+++ src/main/java/dispatchApp/controller/LoginController.java
@@ -1,11 +1,13 @@
 package dispatchApp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.json.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 
@@ -13,33 +15,41 @@ import dispatchApp.model.User;
 import dispatchApp.service.UserService;
 
 
-
+@CrossOrigin("*")
 @Controller
-
 public class LoginController {
-
 	@Autowired
 	private UserService userService;
 
 	
 
 	@RequestMapping(value = "/login")
-	public ResponseEntity<?> login(@RequestParam(value = "email")  String userEmail, @RequestParam(value = "password") String password ) {
-		
-		User userExist = userService.gerUserByName(userEmail);
-		String passwordExist = userExist.getAccount().getPassword();
+	public ResponseEntity<?> login(@RequestBody String json ) {
+		System.out.println("new login received!!!!!!!!!received!!!!!!!!!received!!!!!!!!!received!!!!!!!!!received!!!!!!!!!received!!!!!!!!!");
+		JSONObject jsonObj=new JSONObject(json);
+		String email =  jsonObj.getString("email");
+		String password =  jsonObj.getString("password");
+		User userExist = userService.gerUserByName(email);
+		JSONObject result = new JSONObject();
 		if(userExist == null) {
-			return new ResponseEntity<>("Login failed! User do not exist!", HttpStatus.UNAUTHORIZED);//用户不存在
-		} else if(!password.equals(passwordExist)){
-			return new ResponseEntity<>("Login failed! Password is incorrect!",HttpStatus.UNAUTHORIZED);// 密码错误	
-		}
-		return new ResponseEntity<>(userService.gerUserByName(userEmail),HttpStatus.OK);// 以json格式将user给前端；
-	}
+			result.put("fail1","Login failed! User do not exist!");
+			return new ResponseEntity<>(result.toString(), HttpStatus.UNAUTHORIZED);
+		} else if(!password.equals(userExist.getAccount().getPassword())){
+			result.put("fali2","Login failed! Password is incorrect!");
+			return new ResponseEntity<>(result.toString(),HttpStatus.UNAUTHORIZED);}
+		result.put("email", email);
+		result.put("firstName", userExist.getFirstName());
+		result.put("lastName", userExist.getLastName());
+		return new ResponseEntity<>(result.toString(),HttpStatus.OK);
+	    }
 	
 	@RequestMapping(value = "/logout")
 	public ResponseEntity<String> logout(){
-		return new ResponseEntity<>("Logout successful.", HttpStatus.OK);
+		System.out.println("new logout received!!!!!!!!!received!!!!!!!!!received!!!!!!!!!received!!!!!!!!!received!!!!!!!!!received!!!!!!!!!");
+		
+		JSONObject result = new JSONObject();
+		result.put("logout", "Logout successful.");
+		return new ResponseEntity<>(result.toString(), HttpStatus.OK);
 		
 	}
 }
-
